96dfeab6838371421b040259dac25306e7266e60
sa3047 <swapnil_aher@yahoo.co.in>
2016-08-16 15:51:47 -0400

    Initial Commit

---
+++ Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="NA">
+//     All rights reserved.
+// </copyright>
+namespace Practice
+{
+    using SQLPractice;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDataUsingSql obj = new GetDataUsingSql();
+            obj.ConnectionStr = @"Server=SWAPNIL\SQLEXPRESS;Database=NORTHWIND;Trusted_connection=true";
+
+            var result = obj.GetProducts();
+
+        }
+    }
+}
